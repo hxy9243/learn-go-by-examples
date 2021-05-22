f5a676cf5e5c0cd4920623d199a4e1c0ce0c9716
Kevin Hu <hxy9243@gmail.com>
2021-05-22 18:01:04 -0500

    add context example

---
+++ src/context.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+)
+
+func workload(ctx context.Context) error {
+	t := time.NewTicker(3 * time.Second)
+	log.Printf("Start work at %v", time.Now())
+
+	for {
+		select {
+		case now := <-t.C:
+			log.Printf("Still working at %v", now)
+		case <-ctx.Done():
+			log.Printf("Received cancel signal, exiting...")
+
+			// when handling cancel signals, remember to properly close resources
+			// to avoid resource leaks
+			t.Stop()
+
+			return errors.New("user cancelled")
+		}
+
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.TODO(), 7*time.Second)
+	defer cancel()
+
+	if err := workload(ctx); err != nil {
+		log.Printf("Error: %s", err)
+	}
+}
